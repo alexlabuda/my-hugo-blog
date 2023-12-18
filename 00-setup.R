430cc46915af4daba0e11a244cab6844e2b8e4e9
Alex Labuda <alexlabuda@gmail.com>
2023-12-18 09:05:57 -0500

    initial commit

---
+++ 00-setup.R
@@ -0,0 +1,17 @@
+# Libraries
+library(blogdown)
+
+# # Run to create new blog website resources
+
+# new_site(theme = "hugo-apero/hugo-apero", 
+#            format = "toml",
+#            sample = FALSE,
+#            empty_dirs = TRUE)
+
+
+
+# To preview site, run:
+blogdown::serve_site()
+
+# To stop preview, run:
+blogdown::stop_server()
